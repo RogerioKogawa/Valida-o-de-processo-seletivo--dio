2677359c175e2b8c471cb6b1f4756baa4af6624c
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-04-12 21:32:46 -0300

    Adicionando as entradas

--- src/Contador.java
+++ src/Contador.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int primeiroNumero = Integer.parseInt(input.nextLine());
+
+        System.out.println("Digite o segundo número: ");
+        int segundoNumero = Integer.parseInt(input.nextLine());
     }
 }
