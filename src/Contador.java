80123508a41128e1c0517c7ffad38c2c6bf6ff92
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-04-15 10:31:31 -0300

    Adcionando o NumberFormatException(sem conflito)

--- src/Contador.java
+++ src/Contador.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Digite o primeiro número: ");
-        int primeiroNumero = Integer.parseInt(input.nextLine());
-
-        System.out.println("Digite o segundo número: ");
-        int segundoNumero = Integer.parseInt(input.nextLine());
-
         try{
+            System.out.println("Digite o primeiro número: ");
+            int primeiroNumero = Integer.parseInt(input.nextLine());
+
+            System.out.println("Digite o segundo número: ");
+            int segundoNumero = Integer.parseInt(input.nextLine());
+
             contar(primeiroNumero,segundoNumero);
-        }catch (Exception e){
+        }catch (ParametrosInvalidosException e){
             System.out.println("Erro: " + e.getMessage());
+        }catch(NumberFormatException e){
+            System.out.println("Formato de número inválido: " + e.getMessage());
         }
     }
     static void contar(int primeiroParametro, int segundoParametro) throws ParametrosInvalidosException{
