d3c0b4d34e9e098da47e51942bf8500347642513
Rogerio Kogawa <rogeriokogawa@gmail.com>
2025-04-15 10:23:31 -0300

    Adicionando NumberFormatException

--- src/Contador.java
+++ src/Contador.java
@@ -4,17 +4,20 @@ public class Contador {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro número: ");
-        int primeiroNumero = Integer.parseInt(input.nextLine());
+        try {
+            System.out.println("Digite o primeiro número: ");
+            int primeiroNumero = Integer.parseInt(input.nextLine());
 
-        System.out.println("Digite o segundo número: ");
-        int segundoNumero = Integer.parseInt(input.nextLine());
+            System.out.println("Digite o segundo número: ");
+            int segundoNumero = Integer.parseInt(input.nextLine());
 
-        try{
-            contar(primeiroNumero,segundoNumero);
-        }catch (Exception e){
+            contar(primeiroNumero, segundoNumero);
+        }catch (NumberFormatException e) {
+            System.out.println("Um dos parametros está inválido");
+        }catch (ParametrosInvalidosException e) {
             System.out.println("Erro: " + e.getMessage());
         }
+
     }
     static void contar(int primeiroParametro, int segundoParametro) throws ParametrosInvalidosException{
         int contagem = segundoParametro - primeiroParametro;
